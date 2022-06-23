a0f808b605697de59d8d52a90833b36d3a819ba7
박원호 <36305517+olivyou2@users.noreply.github.com>
2022-06-24 02:02:56 +0900

    feat: cors

--- src/route/post/index.js
+++ src/route/post/index.js
@@ -1,15 +1,25 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import deleteRoute from './delete';
-import getRoute from './get';
-import postRoute from './post';
-import putRoute from './put';
+import deleteRoute from "./delete";
+import getRoute from "./get";
+import postRoute from "./post";
+import putRoute from "./put";
 
+import cors from "cors";
+
+const postRouter = Router();
 const router = Router();
 
 router.use(getRoute);
 router.use(postRoute);
 router.use(putRoute);
 router.use(deleteRoute);
+
+postRouter.use(
+  cors({
+    origin: ["http://localhost:80"],
+  }),
+  router
+);
 
 export default router;
